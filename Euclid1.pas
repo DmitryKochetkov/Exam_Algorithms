36a13a2746faaf02087ee149b55332d7f384e412
DmitryKochetkov <34890452+DmitryKochetkov@users.noreply.github.com>
2019-02-22 15:06:15 +0300

    Euclid Algrorithm: subtraction

---
+++ Euclid1.pas
@@ -0,0 +1,16 @@
+﻿var
+   x1, x2: integer;
+   
+function Euclid(a, b: integer): integer;
+begin
+   while a<>b do
+   if a>b then a:=a-b else b:=b-a;
+   Euclid := a;
+end;
+
+begin
+   write('Enter 2 natural numbers: ');
+   read(x1, x2);
+   if (x1 <= 0) or (x2 <= 0) then write('Non-natural numbers are not allowed.') 
+   else write(Euclid(x1, x2));
+end.
